a16e8fe078b49cb03859ca0da32f38d65ac52851
Kauã Lima de Souza <74164331+Kaua15@users.noreply.github.com>
2020-12-03 18:58:02 -0300

    Add files via upload

---
+++ public/Js/js1.js
@@ -0,0 +1,39 @@
+function linkenter(){
+    var link1 = window.document.querySelector('#a1')
+    link1.style = `text-decoration: underline #aaaaaa;`
+}
+
+function linkexit(){
+    var link1 = window.document.querySelector('#a1')
+    link1.style = `text-decoration: none;`
+}
+
+function linkenter2(){
+    var link1 = window.document.querySelector('#a2')
+    link1.style = `text-decoration: underline #aaaaaa;`
+}
+
+function linkexit2(){
+    var link1 = window.document.querySelector('#a2')
+    link1.style = `text-decoration: none;`
+}
+
+function linkenter3(){
+    var link1 = window.document.querySelector('#a3')
+    link1.style = `text-decoration: underline #aaaaaa;`
+}
+
+function linkexit3(){
+    var link1 = window.document.querySelector('#a3')
+    link1.style = `text-decoration: none;`
+}
+
+function bigImg(x) {
+    x.style.height = "64px";
+    x.style.width = "64px";
+}
+  
+function normalImg(x) {
+    x.style.height = "32px";
+    x.style.width = "32px";
+}
